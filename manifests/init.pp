4bf8c5be21c2a366f11dbaa7a0558d44f0d00d32
Foppe Pieters <foppe.pieters@naturalis.nl>
2015-07-20 16:54:41 +0200

    :heart: added test-site for streams

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,12 +6,29 @@ class livestream (
     $channel_id    = undef,
     $api_key       = undef,
 ){
+  # install packages
+  package { 'p7zip-full':
+    ensure                => installed
+  }
   file { '/var/www/htdocs/index.php' :
     content     => template('livestream/index.php.erb'),
     mode        => '0755',
   }
-  file { '/var/www/htdocs/EmbedYoutubeLiveStreaming.php' :
-    content     => template('livestream/EmbedYoutubeLiveStreaming.php.erb'),
-    mode        => '0755',
+  # download protected images
+  file {'/tmp/test-stream.zip':
+    ensure                => 'present',
+    source                => 'puppet:///modules/livestream/test-stream.zip',
+    mode                  => '0755',
+    owner                 => 'kiosk',
+    group                 => 'kiosk',
+    notify                => Exec['site-unzip']
+  }
+
+  exec {'site-unzip':
+    command               => '/usr/bin/7z x -aoa /tmp/test-stream.zip',
+    cwd                   => '/var/www/htdocs/',
+    unless                => '/usr/bin/test -f /var/www/htdocs/css',
+    refreshonly           => true,
+    require               => File['/tmp/test-stream.zip'],
   }
 }
